82115b7578d0f5339d7680e67d626063b5c40d84
Lucas Torres <repositoriolucas@gmail.com>
2023-09-25 03:26:55 -0300

    Adicionei mais eventos de click e animações

--- js/script.js
+++ js/script.js
@@ -8,13 +8,27 @@ let mudar_item_ativo = () => {
 
 itens_menu.forEach(item => {
     item.addEventListener("click", ()=>{
+        mudar_item_ativo()
         item.classList.add("itemAtivo")
         if(item.id != "notificacoes") {
             document.querySelector(".notificacoesPopup").style.display = 'none'
         } else {
             document.querySelector(".notificacoesPopup").style.display = "block"
+            document.querySelector("#notificacoes .contagemNotificacoes").style.display = 'none'
         }
     })
 })
 
-console.log(itens_menu)
+// console.log(itens_menu)
+
+// Mensagens
+
+let notificacoes_mensagens = document.querySelector("#notificacaoMensagens")
+let mensagens = document.querySelector(".sectionMensagens")
+
+notificacoes_mensagens.addEventListener("click", ()=>{
+    mensagens.style.boxShadow = '0 0 1rem var(--corPrimaria)'
+    setTimeout(()=>{
+        mensagens.style.boxShadow = 'none'
+    }, 2000)
+})
