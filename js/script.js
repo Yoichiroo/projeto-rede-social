1ab393e72a93b2d8f08f78dfc3f55d31387201a7
Lucas Torres <repositoriolucas@gmail.com>
2023-09-25 03:41:07 -0300

    Barra de pesquisa funcional

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Sidebar
+
 let itens_menu = document.querySelectorAll(".itemMenu")
 
 let mudar_item_ativo = () => {
@@ -19,16 +21,34 @@ itens_menu.forEach(item => {
     })
 })
 
-// console.log(itens_menu)
-
 // Mensagens
 
 let notificacoes_mensagens = document.querySelector("#notificacaoMensagens")
 let mensagens = document.querySelector(".sectionMensagens")
 
 notificacoes_mensagens.addEventListener("click", ()=>{
-    mensagens.style.boxShadow = '0 0 1rem var(--corPrimaria)'
+    mensagens.style.boxShadow = '0 0 0.5rem var(--corPrimaria)'
+    notificacoes_mensagens.querySelector(".contagemNotificacoes").style.display = 'none'
     setTimeout(()=>{
         mensagens.style.boxShadow = 'none'
     }, 2000)
 })
+
+// Filtrando mensagens
+
+let mensagem = mensagens.querySelectorAll('.mensagem')
+let search_box_mensagem = document.getElementById("searchMensagem")
+let procurar_mensagem = () => {
+    let valor = search_box_mensagem.value.toLowerCase()
+    console.log(valor)
+    mensagem.forEach(chat => {
+        let nome = chat.querySelector("h5").textContent.toLocaleLowerCase()
+        if(nome.indexOf(valor) != -1) {
+            chat.style.display ='flex'
+        } else {
+            chat.style.display = 'none'
+        }
+    })
+}
+
+search_box_mensagem.addEventListener("keyup", procurar_mensagem)
